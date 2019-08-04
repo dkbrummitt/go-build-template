f13e39873fb7b7e2da750bcd5e991ff90770162c
dkbrummitt <dkbrummitt@gmail.com>
2019-08-04 11:19:06 -0700

    code cleanup.

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2016 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -25,12 +9,12 @@ import (
 )
 
 func main() {
-	//sample of version data that can be logged or sent to monitoring/usage-tracking
+	// sample of version data that can be logged or sent to monitoring/usage-tracking
 	fmt.Println("MyApp", version.GetVersion())
 
-	//Sample pull of stats data that can be logged or sent to monitoring
+	// Sample pull of stats data that can be logged or sent to monitoring
 	fmt.Println("System Stats:")
-	st := stats.NewStats(stats.StatsOptions{})
+	st := stats.NewStats(stats.Options{})
 	st.PullStats()
 	// b, err := json.Marshal(st)
 	b, err := json.MarshalIndent(st, "", "  ")
