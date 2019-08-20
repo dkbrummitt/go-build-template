7acac44099ca7088cdd439cec74508183164201e
dkbrummitt <dkbrummitt@gmail.com>
2019-08-20 04:26:58 -0700

    code cleanup. removed commented out code that was copy-paste. improve comment

--- pkg/data/options.go
+++ pkg/data/options.go
@@ -1,28 +1,7 @@
 package data
 
 type Options interface {
-	// Rule of thumb, accept interfaces (or bytes)
-	// return structs*
+	// Rule of thumb, accept interfaces (or bytes) as parameters
+	// return struct, bools, (concrete types)
 	Validate() (bool, error)
 }
-
-/*
-//
-type dataImpl struct {
-	Create() (interface{}, error){
-		// Place imagination here
-	}
-	Retrieve() (interface{}, error){
-		// Place imagination here
-	}
-	Update() (interface{}, error){
-		// Place imagination here
-	}
-	Delete() (interface{}, error){
-		// Place imagination here
-	}
-}
-func NewData() Data {
-  return &dataImpl
-}
-*/
